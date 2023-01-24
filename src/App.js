5aba2ca52eee7a241cd8fa50378b531134863179
B-Forest <benjamin.frt28@gmail.com>
2023-01-25 00:12:00 +0100

    ajout de style page d'acceuil

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Text,
   VStack,
   Grid,
-
+  
   Button,
   Heading,
   Flex,
@@ -28,9 +28,9 @@ function App() {
                 <Heading>Home Page</Heading>
               <Text> Voici les liens de nos différentes fonctionalités </Text>
               <Text>Vous pouvez avec cette application rechercher le profile d'un jouer et aussi afficher le classement de chaque divisions</Text>
-              <Flex minWidth='max-content' direction='column' justify='space-around'  gap='2'>
-                <Link className="nav-item" to='/rankingsearch'><Button colorScheme='blue'>Ranking</Button></Link>
-                <Link className="nav-item" to='/profilesearch'><Button colorScheme='blue'>Profile</Button></Link>
+              <Flex minWidth='max-content' direction='column' justify='space-around'  gap='50' m='20'>
+                <Link className="nav-item" to='/rankingsearch'><Button colorScheme='blue' size='lg'>Ranking</Button></Link>
+                <Link className="nav-item" to='/profilesearch'><Button colorScheme='blue' size='lg'>Profile</Button></Link>
               </Flex>
               </div>
           </div>
